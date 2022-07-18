ce6b8689bc5bd947f607288c2ad93a0d02cfa195
yasirrifai <76211378+yasirrifai@users.noreply.github.com>
2022-07-18 14:46:44 +0530

    Added a Procfile.

--- app.js
+++ app.js
@@ -142,11 +142,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-
 let port = process.env.PORT;
-if(port == null || port = ""){
+if (port == null || port == "") {
   port = 3000;
 }
+
 app.listen(port, function() {
   console.log("Server started on port 3000");
 });
